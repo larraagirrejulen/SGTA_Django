c7b84be4d87c3efcb4e25a23784d026045f354c3
Julen <larraagirrejulen@gmail.com>
2022-03-31 13:52:59 +0200

    a

--- django_larranaga/django_larranaga/FilmenGunea/views.py
+++ django_larranaga/django_larranaga/FilmenGunea/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login as auth_login
 
 
 def index(request):
     form = LoginForm()
     return render_to_response('FilmenGunea/index.html', {'form':form})
 
+def login(request):
+    erab = request.POST['username']
+    pasa = request.POST['password']
+    user = authenticate(username=erab, password=pasa)
+    if user is not None:
+        if user.is_active:
+            auth_login(request, user)
+            # Berbideratu login ondorengo orri batera.
+            return HttpResponseRedirect('menus/')
+        else:
+            # Errore-mezua bueltatu: 'kontua desgaituta'.
+            
+    else:
+        # Errore-mezua bueltatu: 'login desegokia'.
+
+
 def erregistratu(request):
     form = RegisterForm()
-    return render_to_response('FilmenGunea/index.html', {'form':form})
+    return render_to_response('FilmenGunea/register.html', {'form':form})
 
 def menua(request):
     return render_to_response('FilmenGunea/menua.html')
