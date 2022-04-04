72aee4421cd8fa9ab3cfbbfb49e818fbbe629aea
Julen <larraagirrejulen@gmail.com>
2022-04-04 10:27:46 +0200

    erregistratzea hasi

--- django_larranaga/django_larranaga/FilmenGunea/urls.py
+++ django_larranaga/django_larranaga/FilmenGunea/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('main/', views.main, name='index'),
     path('register/', views.register, name='index'),
+    path('register_request/', views.register_request, name='index'),
     path('login/', views.login, name='index')
 ]
--- django_larranaga/django_larranaga/FilmenGunea/views.py
+++ django_larranaga/django_larranaga/FilmenGunea/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponse
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
 
 
 def index(request):
@@ -22,15 +23,24 @@ def login_request(request):
             return HttpResponseRedirect('menus/')
         else:
             # Errore-mezua bueltatu: 'kontua desgaituta'.
-            true
+            return HttpResponse("<h1>Errorea: kontua desgaituta dago</h1>")
     else:
         # Errore-mezua bueltatu: 'login desegokia'.
-        true
-
+        return HttpResponse("<h1>Errorea: erabiltzaile edo pasahitz okerra</h1>");
 
 def register(request):
     form = RegisterForm()
     return render_to_response('FilmenGunea/register.html', {'form':form})
+
+def register_request(request):
+    erab = request.POST['username']
+    pasa = request.POST['password']
+    user = authenticate(username=erab, password=pasa)
+    if user is not None:
+        return render_to_response('FilmenGunea/register.html', {'emaitza':"<h1>Errorea: erabiltzailea dagoeneko erregistratuta dago</h1>"})
+    else:
+        erab = User.objects.create_user(erab, pasa)
+        return render_to_response('FilmenGunea/register.html', {'emaitza':"<h1>Erabiltzailea zuzen erregistratu da</h1>"})
 
 def main(request):
     return render_to_response('FilmenGunea/froga.html')
