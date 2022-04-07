893805b218437de5c7d1ede279c65aacee486d1a
Julen <larraagirrejulen@gmail.com>
2022-04-07 12:52:04 +0200

    render_to_response bug fix

--- django_larranaga/django_larranaga/FilmenGunea/views.py
+++ django_larranaga/django_larranaga/FilmenGunea/views.py
@@ -1,27 +1,25 @@
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import LoginForm, RegisterForm
-from django.contrib.auth import authenticate, login as login_auth
+from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
 def index(request):
-    return render_to_response('FilmenGunea/index.html')
+    return render(request, 'FilmenGunea/index.html')
 
 def login(request):
     if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        if authenticate(username, password) is not None:
+        if authenticate(request) is not None:
             if user.is_active:
-                login_auth(request, user)
+                auth_login(request, user)
                 return HttpResponseRedirect('menus/')
             else:
                 messages.error(request, "Ezin izan da kontuan sartu. Kontua desgaituta dago.")
         else:
             messages.error(request, "Ezin izan da kontuan sartu. Erabiltzaile izen edo pasahitz okerra.")
     form = LoginForm()
-    return render_to_response('FilmenGunea/login.html', {'form':form})
+    return render(request, 'FilmenGunea/login.html', {'form':form})
 
 def register(request):
     if request.method == "POST":
@@ -31,12 +29,12 @@ def register(request):
         if password1 == password2:
             user = User.objects.create_user(username, password1, password2)
             user.save()
-            login(request, user)
+            auth_login(request, user)
             messages.success(request, "Zuzen erregistratu zara.")
             return redirect('FilmenGunea/main.html')
         messages.error(request, "Ezin izan da erregistratu. Datuak ez dira baliozkoak.")
     form = RegisterForm()
-    return render_to_response('FilmenGunea/register.html', {'form':form})
+    return render(request, 'FilmenGunea/register.html', {'form':form})
 
 def main(request):
-    return render_to_response('FilmenGunea/froga.html')
+    return render(request, 'FilmenGunea/froga.html')
