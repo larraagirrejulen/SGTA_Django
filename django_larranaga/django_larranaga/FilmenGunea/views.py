0a525f2b3a9eb298330a59d5c67f61a1ed347a59
Julen <larraagirrejulen@gmail.com>
2022-04-09 17:42:53 +0200

    login eta register

--- django_larranaga/django_larranaga/FilmenGunea/views.py
+++ django_larranaga/django_larranaga/FilmenGunea/views.py
@@ -10,11 +10,11 @@ def index(request):
 
 def login(request):
     if request.method == "POST":
-        if authenticate(request) is not None:
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
             if user.is_active:
-                messages.error(request, "a")
                 auth_login(request, user)
-                return HttpResponseRedirect('menus/')
+                return redirect('../main')
             else:
                 messages.error(request, "Ezin izan da kontuan sartu. Kontua desgaituta dago.")
         else:
@@ -28,11 +28,11 @@ def register(request):
         password1 = request.POST['password1']
         password2 = request.POST['password2']
         if password1 == password2:
-            user = User.objects.create_user(username, password1, password2)
+            user = User.objects.create_user(username=username, password=password1)
             user.save()
             auth_login(request, user)
             messages.success(request, "Zuzen erregistratu zara.")
-            return redirect('FilmenGunea/main.html')
+            return redirect('../main')
         messages.error(request, "Ezin izan da erregistratu. Datuak ez dira baliozkoak.")
     form = RegisterForm()
     return render(request, 'FilmenGunea/register.html', {'form':form})
